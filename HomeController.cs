ea4b424d732d68a41f71bb756cb44cca5e9827ca
code4545 <manikanta.dokku@gmail.com>
2019-07-03 08:05:15 -0700

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DeploymentFailureRetroTracker.Models;
+
+
+namespace DeploymentFailureRetroTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+      [HttpGet]
+        public ActionResult GetDetails()
+       {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetDetails(DateTime? FromDate, DateTime? ToDate, int CategoryId, string CategoryName,bool? IsRetro, string ErrorDescription)
+        {
+            return View();
+
+        }
+        //public ActionResult GetDetails()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult GetDetails(Dfrt failure)
+        //{
+        //    if(ModelState.IsValid)
+        //    {
+        //        using (OurDbContext db = new OurDbContext())
+        //        {
+        //            db.form.Add(failure);
+        //            db.SaveChanges();
+        //        }
+        //        ModelState.Clear();
+        //    }
+        //    return View();
+        //}
+        ////display
+        //public ActionResult DisplayDetails()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult DisplayDetails(Dfrt user )
+        //{
+        //    //using (OurDbContext db = new OurDbContext())
+        //    //{
+
+        //    //}
+        //        return View();
+        //}
+    }
+
+}
+
+   
--- dfrt.js
+++ dfrt.js
@@ -10,116 +10,145 @@
         value: new Date(),
         dateInput: true
     });
-    
-   
-    $("#btnsubmit").click(function () {
-        var date = $(frmdatetimepicker).data("kendoDateTimePicker").value();
-        alert(date);
-    });
     $("btnrefresh").click(function () {
 
-    })
-    $("#color").kendoDropDownList({
+    });
+    $("#Category").kendoDropDownList({
      
+    });
+   
+    $("#btnsubmit").click(function ()
+    { 
+        var date = $("#frmdatetimepicker").data("kendoDateTimePicker").value();
+      
+        //var date1 = $("#todatetimepicker").data("kendoDateTimePicker").value();
+        $.ajax({   
+            type: "post",   
+            url: "/Home/GetDetails",   
+            data: JSON.stringify(Dfrt),   
+            datatype: "json",   
+            traditional: true,   
+            success: function (data) {   
+                // debugger   
+                if (data == "No Record Found") {   
+                    alert('No Record Found');   
+                }   
+                else {   
+                    $('#gridretro').html(data);   
+                    
+                }   
+            }   
+        });   
     });   
-});
-var kendoUIHelper = {
-    generatekendogrid: function () {
+       
+    });
+
+
+    var kendoUIHelper = {
+        generatekendogrid: function () {
         
    
-$("#gridretro").kendoGrid({
-    //scrollable: true,
-    //sortable: true,
-    //pageable: {
-    //    refresh: false,
-    //    pageSizes: [5, 10, 20, 30],
-    //    buttonCount: 5
-    //},
-    //filterable: true,
-    navigatable: true,
-    editable: "inline",
-    columns: [
-        {
-            field: "EmailAddress",
-            title: "Dep.ID",
-        },
-        {
-            field: "Env",
-            title: "Env",
+            $("#gridretro").kendoGrid({
+                //scrollable: true,
+                //sortable: true,
+                //pageable: {
+                //    refresh: false,
+                //    pageSizes: [5, 10, 20, 30],
+                //    buttonCount: 5
+                //},
+                //filterable: true,
+                navigatable: true,
+                editable: "inline",
+                columns: [
+                    {
+                        field: "EmailAddress",
+                        title: "Dep.ID",
+                    },
+                    {
+                        field: "Env",
+                        title: "Env",
           
-        },
-        {
-            field: "ModifiedBy",
-            title: "Build No.",
-            width : "6em",
+                    },
+                    {
+                        field: "ModifiedBy",
+                        title: "Build No.",
+                        width : "6em",
             
-        },
-        {
-            field: "ModifiedOn",
-            title: "x ",
-            width: "2em",
-        },
-        {
-            field: "ModifiedBy",
-            title: "y",
-            width: "2em",
+                    },
+                    {
+                        field: "ModifiedOn",
+                        title: "x ",
+                        width: "2em",
+                    },
+                    {
+                        field: "ModifiedBy",
+                        title: "y",
+                        width: "2em",
             
-        },
-         {
-             field: "ModifiedBy",
-             title: "z",
-             width: "2em",
+                    },
+                     {
+                         field: "ModifiedBy",
+                         title: "z",
+                         width: "2em",
             
-         },
-          {
-              field: "ModifiedBy",
-              title: "Owner",
+                     },
+                      {
+                          field: "ModifiedBy",
+                          title: "Owner",
             
-          },
-           {
-               field: "ModifiedBy",
-               title: "Status",
+                      },
+                       {
+                           field: "ModifiedBy",
+                           title: "Status",
             
-           },
-            {
-                field: "ModifiedBy",
-                title: "Retro",
+                       },
+                        {
+                            field: "ModifiedBy",
+                            title: "Retro",
             
-            },
-             {
-                 field: "ModifiedBy",
-                 title: "BugID",
+                        },
+                         {
+                             field: "ModifiedBy",
+                             title: "BugID",
             
-             },
-              {
-                  field: "ModifiedBy",
-                  title: "Fix",
+                         },
+                          {
+                              field: "ModifiedBy",
+                              title: "Fix",
             
-              },
-               {
-                   field: "ModifiedBy",
-                   title: "Failure",
+                          },
+                           {
+                               field: "ModifiedBy",
+                               title: "Failure",
             
-               },
-                {
-                    field: "ModifiedBy",
-                    title: "Category",
+                           },
+                            {
+                                field: "ModifiedBy",
+                                title: "Category",
             
-                },
-                 {
-                     field: "ModifiedBy",
-                     title: "Last Modified",
-                     width: "8em",
+                            },
+                             {
+                                 field: "ModifiedBy",
+                                 title: "Last Modified",
+                                 width: "8em",
             
-                 },
-                  {
-                      field: "ModifiedBy",
-                      title: "Action",
-                  }
-        ],
+                             },
+                              {
+                                  field: "ModifiedBy",
+                                  title: "Action",
+                              }
+                ],
 
 
-});
+            });
+        }
     }
-}
 
+    function Subdet() {
+        var userModel = {
+            FromDate: $("#frmdatetimepicker").val(),
+            ToDate: $("#todatetimepicker").val(),
+            Category: $("#Category").val(),
+            IsRetro: $("#myCheck").val()
+        }
+    };
