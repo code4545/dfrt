f8be8adf6494db49f85b649655ba87e33b14aba2
code4545 <manikanta.dokku@gmail.com>
2019-07-03 23:10:15 -0700

    Add files via upload

---
+++ Dfrt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeploymentFailureRetroTracker.Models
+{
+    public class Dfrt
+    {
+        [DataType(DataType.Date)]
+        public DateTime? FromDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? ToDate { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        [Required(ErrorMessage ="Is Retro is mandatory")]
+        public bool? IsRetro {get;set;}
+
+        public string ErrorDescription { get; set; }
+        
+    }
+  
+
+       
+    }
+
